52aac7c5ebd68fbd1a4395164f60dab23637374e
olimowham-cpu <olimowham@gmail.com>
2025-12-23 23:24:25 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,25 @@
+// See https://aka.ms/new-console-template for more informatio
+
+
 using System;
 
 class Program
 {
     static void Main()
     {
-        int a = int.Parse(Console.ReadLine());
-        int b = int.Parse(Console.ReadLine());
-        int c = int.Parse(Console.ReadLine());
-        int d = 0;
-        if (a <0)
-        {
-            d += 1;
-       
-        }
-         if (b< 0)
-        {
-            
-            d += 1;
-        }
-        if (c < 0)
-        {
-            
-            d += 1;
-        }
 
-        Console.WriteLine(d);
-        
-}
+
+        int a = int.Parse(Console.ReadLine());
+
+        if (a>0)
+            
+        {
+            Console.WriteLine(a+1);
+            
+        }
+        else
+        {
+            Console.WriteLine(a);
+        }
+    }
 }

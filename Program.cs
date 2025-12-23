efb3277e4b5fee5c4e4e69f5f96f7a13b442dd56
olimowham-cpu <olimowham@gmail.com>
2025-12-23 22:46:21 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class program
+{
+    static void main()
+    {
+        int A = -1;
+        int B = -5;
+
+        bool result = (A >= 0) || (B < -2);
+
+        Console.WriteLine(result);
+
+    }
+}

abe92b8a0c62e36502f59661ad39c12e88b4bc35
olimowham-cpu <olimowham@gmail.com>
2025-12-23 23:25:47 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,24 +4,28 @@ class program
 {
     static void Main()
     {
-        int a = int.Parse(Console.ReadLine());
-        int b = int.Parse((Console.ReadLine()));
-        int c = 0;
-
+        int a =50;
+        int b = 20;
+        int c = 10;
         if (a < b)
         {
             Console.WriteLine(a);
-            Console.WriteLine(b);
         }
 
-        if (b < a)
+        else if(b<a)
+            
         {
             Console.WriteLine(b);
-            Console.WriteLine(a);
         }
         else
         {
-            Console.WriteLine("oshipka");
+            Console.WriteLine(c);
         }
+
+
+   
+
+
+
     }
 }

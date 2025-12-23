55c3293c971404bfb6f946e27d140f75eb96f256
olimowham-cpu <olimowham@gmail.com>
2025-12-23 23:19:14 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,14 +4,12 @@ class program
 {
     static void main()
     {
+        int A = 3;
+        int B = 7;
 
-
-        int A = 8;
-        int B = 5;
-        int C = 2;
-
-        bool result = (A < B && B < C) || (C < B && B < A);
+        bool result = (A % 2 != 0) && (B % 2 != 0);
 
         Console.WriteLine(result);
+
     }
 }

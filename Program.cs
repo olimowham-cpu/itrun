abcccae8abaf02afd70e5ac8c53f6d1168785743
olimowham-cpu <olimowham@gmail.com>
2025-12-23 23:22:53 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,41 +4,14 @@ class Program
 {
     static void Main()
     {
-        
-        int day = int.Parse(Console.ReadLine());
-
-        string Name;
-
-        switch (day)
+        int x = int.Parse(Console.ReadLine());
+        if (x > 0)
         {
-            case 1:
-                Name = "poni"; 
-                break;
-            case 2:
-                Name = "vtorn";    
-                break;
-            case 3:
-                Name = "sreda";       
-                break;
-            case 4:
-                Name = "chetverg";    
-                break;
-            case 5:
-                Name = "5nitsa";   
-                break;
-            case 6:
-                Name = "subbota";   
-                break;
-            case 7:
-                Name = "voskresenie";
-                break;
-            default:
-                Name = "error";
-                break;
-          
+            Console.WriteLine(x+1);
         }
-        
-
-        Console.WriteLine("  " + Name);
+        else
+        {
+            Console.WriteLine(x-2);
+        }
     }
 }

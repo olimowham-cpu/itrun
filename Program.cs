fb970cfd3e2c2f58a88a2258001dec987f526ec3
olimowham-cpu <olimowham@gmail.com>
2025-12-23 23:10:37 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,15 +4,11 @@ class Program
 {
     static void Main()
     {
-        Console.Write(" R: ");
-        double R = Convert.ToDouble(Console.ReadLine());
+        Console.Write("d: ");
+        double d = Convert.ToDouble(Console.ReadLine());
 
-        const double pi = 3.14; 
-
-        double L = 2 * pi * R;  
-        double S = pi * R * R;   
-
-        Console.WriteLine("d: " + L);
-        Console.WriteLine("П: " + S);
+         double pi = 3.14;  
+        double a = pi * d;       
+        Console.WriteLine("= " + a);
     }
 }
